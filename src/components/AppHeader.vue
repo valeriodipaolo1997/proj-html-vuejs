b87a0c17f0f817afb7db370a55609416c1114c27
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-03 12:34:25 +0200

    add logo and dinamic navbar on header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,57 @@
 <script>
 export default {
     name: 'AppHeader',
+
+    data() {
+        return {
+            activeLink: 0,
+            navItems: ['home', 'about', 'projects', 'services', 'blog', 'contact']
+
+        }
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <div class="header_top d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    <img class="logo_img" src="../assets/img/logotype.png" alt="logo">
+                </div>
 
-<style lang="scss" scoped></style>
+                <div class="menu">
+                    <nav class="navbar navbar-expand navbar-light">
+                        <ul v-for="(item, index) in navItems" class="nav navbar-nav">
+                            <li class="nav-item" @click="activeLink = index">
+                                <a class="nav-link" :class="activeLink === index ? 'active' : ''" href="#"
+                                    aria-current="page">{{ item }}</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    .header_top {
+        li {
+        a {
+            text-transform: uppercase;
+
+            &.active {
+                color: antiquewhite;
+            }
+        }
+        & a.active {
+            border-top: 2px solid;
+        }
+    }
+    }
+    
+}
+</style>
