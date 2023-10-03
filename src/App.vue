47c4ea3fb013d11ee33333ce8a3eb2660dd50d52
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-03 11:54:50 +0200

    create AppHeader and color variables

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script>
-
+import AppHeader from './components/AppHeader.vue'
+export default {
+  name: 'App',
+  data() {
+    return {
+    }
+  },
+  components: {
+    AppHeader
+  }
+}
 </script>
 
+
 <template>
-  
+  <AppHeader />
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
