717277473dbc5935a112ecc228ef0920bc36e54f
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-04 16:01:29 +0200

    add AppHelp components in AppMain

--- src/components/AppHelp.vue
+++ src/components/AppHelp.vue
@@ -14,10 +14,10 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="help_clients container">
         <span>project</span>
         <div class="d-flex justify-content-between">
-            <h1>OUR EXPERT TRUSTED CONSULTANTS <br> HELP CLIENTS</h1>
+            <h1 class="pb-4">OUR EXPERT TRUSTED CONSULTANTS <br> HELP CLIENTS</h1>
             <div>
                 <button>freccia</button>
                 <button>freccia</button>
@@ -36,15 +36,62 @@ export default {
                 <img class=".img-fluid" src="../assets/img/z1el4c4p-790x592.jpg" alt="">
             </div>
         </div>
+
+        <h6 class="d-flex justify-content-center">TESTIMONIAL</h6>
+        <h1 class="d-flex justify-content-end text-end pb-5">WHAT'S THE CUSTOMERS SAY <br> ABOUT YOUR WORK</h1>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col px-5">
+                    <p>
+                        When, while lovely valley teems whit vapour around meand eridian sun strikes the upper impenetrable
+                        foliage of my trees, and but a When, while lovely valley teems whit vapor around meand eridian sun
+                        strikes. 
+                    </p>
+                  <h4 class="pt-4">Vera Duncan</h4>
+                  <p>Amazon Inc.</p>
+                </div>
+                <div class="col px-5">
+                    <p>
+                        When, while lovely valley teems whit vapour around meand eridian sun strikes the upper impenetrable
+                        foliage of my trees, and but a When, while lovely valley teems whit vapor around meand eridian sun
+                        strikes.
+                    </p>
+                    <h4 class="pt-4">Vera Duncan</h4>
+                    <p>Amazon Inc.</p>
+                </div>
+                <div class="col px-5">
+                    <p>
+                        When, while lovely valley teems whit vapour around meand eridian sun strikes the upper impenetrable
+                        foliage of my trees, and but a When, while lovely valley teems whit vapor around meand eridian sun
+                        strikes. 
+                    </p>
+                    <h4 class="pt-4">Vera Duncan</h4>
+                    <p>Amazon Inc.</p>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-
 img {
-    
+
     max-width: 100%;
 }
 
+.help_clients {
+    padding: 8rem 0;
+}
+
+h6 {
+    padding-top: 6rem;
+}
+
+p {
+    font-size: 0.9rem;
+}
 </style>
